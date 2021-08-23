35e5f627cbb1fb2a406827eb2fda6a0534f2a5a1
Lazar Bojičić <34066720+lazarbojicic@users.noreply.github.com>
2021-08-23 12:48:47 +0200

    update sa zagradama

--- _src/P03/Zagrade.py
+++ _src/P03/Zagrade.py
@@ -1,10 +1,10 @@
 s = input("Unesi string")
-br_otvorenih = 0
-br_zatvorenih = 0
+br_jemanje = 0
+br_jevece = 0
 for x in s:
-    if x == "(": br_otvorenih += 1
-    if x == ")": br_zatvorenih += 1
-if br_otvorenih == br_zatvorenih:
-    print("isti je broj otvorenih i zatvorenih zagrada")
+    if x == "<": br_jemanje += 1
+    if x == ">": br_jevece += 1
+if br_jemanje == br_jevece:
+    print("isti je broj znakova < и >")
 else:
-    print("nije isti broj otvorenih i zatvorenih zagrada")
+    print("nije isti broj znakova < и >")
